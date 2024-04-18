df1578af4f6f7e56d715cb381ab5895d1d0ae542
KaterinaNakou3 <katerina.nakou.2003@gmail.com>
2024-04-18 23:19:26 +0300

    Add src folder

---
+++ LibraryProject/src/main/java/library/Book.java
@@ -0,0 +1,41 @@
+package library;
+
+class Book {
+
+	private String title;
+	private String writer;
+	private int yearPublished;
+	private String summary;
+
+	public Book(String title, String writer, int yearPublished, String summary) {
+		this.title = title;
+		this.writer = writer;
+		this.summary = summary;
+		this.yearPublished = yearPublished;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getYearPublished() {
+		return yearPublished;
+	}
+	public void setYearPublished(int yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+}
---
+++ LibraryProject/src/test/java/library/TestLibrary.java
@@ -0,0 +1,97 @@
+package library;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class TestLibrary {
+
+    private Library library = new Library();
+    private Book book;
+    private HashMap<Integer,Book> books = new HashMap<>();
+    
+    
+    @Before
+    public void setUp() {
+        books.clear();
+        String input = "Me before you\nJojo Moyes\n2012\nA dramatic love story";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        library.addBook();
+    }
+    
+    @Test
+    public void testFindBookSuccess() {
+        setUp();
+        assertEquals(1, library.findBook("Me before you", "Jojo Moyes"));
+        assertEquals(1, library.findBook(null, "Jojo Moyes"));
+
+    }
+
+    @Test
+    public void testFindBookFail() {
+        setUp();
+        assertEquals(0, library.findBook("hihih", null));
+        assertEquals(0, library.findBook(null, "hoho"));
+    }
+
+    /* 
+    @Test 
+    public void testReadBook() {
+        setUp();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        library.readBook("Me before you", null);
+        String output = "Title: " + "Me before you" + "\n"
+        + "Author: " + "Jojo Moyes" + "\n"
+        + "Year of publication: " + "2012" + "\n"
+        + "Summary: " + "A dramatic love story" + "\n";
+        assertEquals(output, out.toString());
+
+        library.readBook(null, "Jojo Moyes");
+        output = "Title: " + "Me before you" + "\n"
+        + "Author: " + "Jojo Moyes" + "\n"
+        + "Year of publication: " + "2012" + "\n"
+        + "Summary: " + "A dramatic love story" + "\n";
+        assertEquals(output, out.toString());
+
+	}
+    */
+    
+    @Test
+    public void TestDeleteBook() {
+        setUp();
+        assertEquals(1, library.deleteBook("Me before you", null));
+        assertEquals(0, library.deleteBook("blahblahblah", null));
+    }  
+
+	public void testAddBook() {
+        books.clear();
+        String input = "Blah\nMe\n1897\nmoreblahhh\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertEquals(1, library.addBook());
+
+        Book newBook = books.get(1);
+        assertEquals("Blah", newBook.getTitle());
+        assertEquals("Me", newBook.getWriter());
+        assertEquals(1897, newBook.getYearPublished());
+        assertEquals("moreblahhh", newBook.getSummary());
+	}
+
+    @Test
+    public void testUpdateBook() {
+        setUp();
+        // Prepare input for simulating user interaction
+        String input = "yes\nNew Title\nyes\nNew Author\nyes\n2020\nyes\nNew Summary\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // Verify that the method returns 1 indicating success
+        assertEquals(1, library.updateBook("Me before you", null));
+
+    }
+
+}
